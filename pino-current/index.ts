f646aab48e285bd97224f93a30c32e800a4e5114
Zhonghua Yan <zhonghua.yan@compass.com>
2023-07-12 18:15:48 +0800

    update: reproduce locally

--- pino-current/index.ts
+++ pino-current/index.ts
@@ -1,4 +1,5 @@
 import dd_trace from "dd-trace";
+import { HttpError } from "http-errors";
 
 dd_trace.init({
   logInjection: true,
@@ -6,17 +7,71 @@ dd_trace.init({
 // dd_trace.use("http");
 
 const pino = require("pino");
-// require("pino-pretty");
+require("pino-pretty");
 
-const logger = pino({
-  level: "info",
-  // prettyPrint: true,
-});
+const hasPretty = true;
 
+// const LOG_FORMAT = "pretty";
+// const stream = undefined;
+const LOG_FORMAT = "json";
+const stream = pino.extreme();
+
+const LOG_FLUSH_INTERVAL = "2500";
+
+console.log(parseInt(LOG_FLUSH_INTERVAL, 10));
+
+const logger = pino(
+  {
+    level: "info",
+    messageKey: "message",
+    base: null,
+    timestamp: true,
+    levelKey: "status",
+    useLevelLabels: true,
+    stream,
+    prettyPrint: hasPretty
+  },
+  pino.destination("/opt/datadog-agent/logs/nodejs.log")
+);
+
+if (stream) {
+  setTimeout(() => logger.flush(), 500).unref(); // Initial flush
+  setInterval(() => logger.flush(), parseInt(LOG_FLUSH_INTERVAL, 10)).unref(); // Periodic
+}
+
+const koa = require("koa");
 dd_trace.trace("test-trace", () => {
-  try {
-    throw new Error("Error Message");
-  } catch (e) {
-    logger.error(e, "error desc");
-  }
+  const app = new koa();
+
+  app.use(async function (ctx: any, next: any) {
+    try {
+      await next();
+    } catch (err) {
+      ctx.status = (err as any).status || 500;
+      ctx.type = "html";
+      ctx.body = "<p>Something <em>exploded</em>, please contact Maru.</p>";
+
+      // since we handled this manually we'll
+      // want to delegate to the regular app
+      // level error handling as well so that
+      // centralized still functions correctly.
+      ctx.app.emit("error", err, ctx);
+    }
+  });
+
+  // response
+
+  app.use(async function () {
+    throw new Error("boom boom");
+  });
+
+  // error handler
+
+  app.on("error", function (err: any, ctx: any) {
+    // logger.error({err, ctx}, "error desc");
+    logger.error("error desc", { err, ctx });
+    logger.error({ err, ctx }, "error desc");
+  });
+
+  app.listen(3000);
 });
